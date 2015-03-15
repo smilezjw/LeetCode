10155a92a8ca8be7e3dd320f8419cf8b20a5c449
smilezjw <smilezjw@qq.com>
2015-03-15 14:30:37 +0800

    Dynamic Programming

--- P97_Interleaving String/Interleaving String.py
+++ P97_Interleaving String/Interleaving String.py
@@ -24,6 +24,7 @@ class Solution:
         len3 = len(s3)
         if len1 + len2 != len3:
             return False
+        # dp[i][j]表示s1[0...i-1]和s2[0...j-1]是否可以拼接为s3[i+j-1]
         dp = [[False for i in xrange(len2+1)] for i in xrange(len1+1)]
         dp[0][0] = True
         for i in xrange(1, len1+1):
@@ -32,6 +33,7 @@ class Solution:
             dp[0][j] = dp[0][j-1] and s2[j-1] == s3[j-1]
         for i in xrange(1, len1+1):
             for j in xrange(1, len2+1):
+                # 如果s1的第i项是s3的第i+j-1项，或者s2的第j项是s3的第i+j-1项
                 dp[i][j] = dp[i-1][j] and s1[i-1] == s3[i+j-1] or dp[i][j-1] and s2[j-1] == s3[i+j-1]
         return dp[len1][len2]
 
@@ -40,3 +42,8 @@ if __name__ == '__main__':
     print s.isInterleave('def', '', 'de')
     print s.isInterleave('aabcc', 'dbbca', 'aadbbcbcac')
     print s.isInterleave('aabcc', 'dbbca', 'aadbbbaccc')
+
+######################################################################################
+# 这道题用动态规划做。一开始用深度优先搜索递归求解，遇到大数据集会超时。
+# 动态转移方程，dp[i][j]表示s1[0...i-1]和s2[0...j-1]是否可以拼接为s3[i+j-1]。
+#
