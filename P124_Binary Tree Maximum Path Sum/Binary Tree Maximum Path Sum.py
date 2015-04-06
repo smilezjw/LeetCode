0978eddac50ffa50cb0240ddc47039ac02592bdf
smilezjw <smilezjw@qq.com>
2015-04-06 14:59:54 +0800

    DFS, Tree

--- P124_Binary Tree Maximum Path Sum/Binary Tree Maximum Path Sum.py
+++ P124_Binary Tree Maximum Path Sum/Binary Tree Maximum Path Sum.py
@@ -21,19 +21,18 @@ class Solution:
         if root is None:
             return 0
         sum = root.val
-        leftMax = 0
-        rightMax = 0
+        leftMax = rightMax = 0
         if root.left:
-            leftMax = self.dfs(root.left)
+            leftMax = self.dfs(root.left)  # 深度递归，得到左子树的最大值
             if leftMax > 0:
                 sum += leftMax
         if root.right:
-            rightMax = self.dfs(root.right)
+            rightMax = self.dfs(root.right)  # 深度递归，得到右子树的最大值
             if rightMax > 0:
                 sum += rightMax
         if sum > Solution.max:
-            Solution.max = sum
-        return max(root.val, max(root.val+leftMax, root.val+rightMax))
+            Solution.max = sum  # 全局变量记录路径的最大和
+        return max(root.val, max(root.val + leftMax, root.val + rightMax))
 
 
 if __name__ == '__main__':
@@ -43,3 +42,9 @@ if __name__ == '__main__':
     root.right = TreeNode(3)
     s = Solution()
     print s.maxPathSum(p)
+
+#######################################################################################
+# 这道题的意思是在树中找到一条路径使得该路径上的节点和最大，起点和终点只要是树中的节点
+# 即可。但是节点值可能为负。
+# 深度递归，用全局变量记录每个子树的最大路径和。
+#
