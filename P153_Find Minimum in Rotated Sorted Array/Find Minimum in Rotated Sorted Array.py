92c47fd2e12efbf46ef60c71f600f2803712a361
smilezjw <smilezjw@qq.com>
2015-04-14 16:23:42 +0800

    Binary Search Array

--- P153_Find Minimum in Rotated Sorted Array/Find Minimum in Rotated Sorted Array.py
+++ P153_Find Minimum in Rotated Sorted Array/Find Minimum in Rotated Sorted Array.py
@@ -24,12 +24,13 @@ class Solution:
     def findMin_Second(self, num):
         left = 0
         right = len(num) - 1
-        while left < right and num[left] > num[right]:  # 旋转的序列满足num[left] > num[right
+        # 旋转的序列满足num[left] > num[right],当不满足这个条件时，num[left]就是最小值
+        while left < right and num[left] > num[right]:
             mid = (left + right) / 2
             if num[mid] < num[right]:  # 旋转的部分在左边
-                right = mid
+                right = mid            # 这里right不是指向mid - 1是因为mid很可能指向最小值
             else:                      # 旋转的部分在右边
-                left = mid + 1
+                left = mid + 1         # 这里left直接指向mid+1是因为mid指向的值已经比right的值大了，不可能是最小值
         return num[left]
 
 if __name__ == '__main__':
@@ -43,4 +44,6 @@ if __name__ == '__main__':
 # 这道题采用二叉查找来求解，我采用的思路是通过判断num[left] <= num[mid] <= num[right]或者
 # num[right] >= num[mid] >= num[left]， 判断是否是递增或者递减序列，然后求得最小值；通过
 # 二分查找改变left和right值。
+# 第二种思路还是很巧妙的，通过判断num[left] > num[right]，如果不满足这个条件则left指向最小
+# 值，因为旋转列表的话一定是满足这个条件的。
 #
