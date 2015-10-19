9e462a2f13143c14b90c7a656e847174fda99bef
smilezjw <smilezjw@qq.com>
2015-10-19 16:15:22 +0800

    update

--- P50_Pow(x,n)/Pow(x,n).py
+++ P50_Pow(x,n)/Pow(x,n).py
@@ -24,6 +24,8 @@ class Solution:
     def pow(self, x, n):  # 采用递归，类似于二分的思路，多么brilliant的做法
         if n == 0:
             return 1.0
+        elif x == 0 and n < 0:
+            return 1.0
         elif n < 0:  # 这里还要考虑n为负数的时候,转化为求1/x的-n次幂
             return self.pow(1/x, -n)
         elif n % 2:  # n为奇数，x的n次幂 = x的n/2次幂 * x的n/2次幂 * x
@@ -36,6 +38,9 @@ if __name__ == '__main__':
     print s.pow(3.0, -10)
     print s.pow(3.0, 10)
     print s.pow(0.00001, 2147483647)
+    print s.pow(0, 0)
+    print s.pow(0, 5)
+    print s.pow(0, -5)
 
 ##########################################################################################
 # 自己写的果然会超时= = 采用递归的方法，类似于二分法的思路，判断n为奇数还是偶数，多么聪明
