3579284782b124917c73690be8ed1ed77fa1df06
Zhang Jiawen <smilezjw@qq.com>
2017-01-12 16:50:35 +0800

    bit manipulation

---
+++ P461_Hamming Distance.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int hammingDistance(int x, int y) {
+        /*
+        int xor = x ^ y;
+        int res = 0;
+        while(xor > 0){
+            res += xor & 1;
+            xor >>= 1;
+        }
+        return res;
+        */
+        return Integer.bitCount(x ^ y);
+    }
+}
